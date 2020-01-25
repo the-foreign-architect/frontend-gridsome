8d6a81dda8071dd30248a52551b0f4c3f8656d85
Daniel da Rocha <danrocha@gmail.com>
2020-01-25 17:20:51 +0800

    implement hotel link component

---
+++ src/components/HotelLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="text-right ">
+    <a :href="link" target="_blank" class="px-3 py-2 text-white bg-black border-2 border-white shadow-lg hover:shadow" :alt="`Check ${name} on Booking.com`"
+      >Check <strong>{{ name }}</strong> on Booking.com &rarr;</a
+    >
+  </p>
+</template>
+
+<script>
+export default {
+  name: "HotelLink",
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ''
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
